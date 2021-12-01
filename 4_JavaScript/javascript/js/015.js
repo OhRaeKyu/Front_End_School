61244b7e8fb8467527c60aba4f9e0c54bcf9e0ab
OhRaeKyu <dhforb1234@naver.com>
2021-12-01 18:02:38 +0900

    2021-12-01

--- 4_JavaScript/javascript/js/015.js
+++ 4_JavaScript/javascript/js/015.js
@@ -3,12 +3,12 @@ function sum(a, b) {
     return a + b;
 }
 
-// 함수 표현식
+// 함수 표현식 : 함수를 따로 선언하지 않고 변수에 대입과 동시에 정의
 let sum = function(a, b) {
     return a + b;
 };
 
-// void 함수
+// void 함수 : return이 없는 함수
 function fx() {
     let x = 10;
     console.log(x);
@@ -30,16 +30,16 @@ function add(a = 10, b = 20) {
 console.log(add(100, 200));
 console.log(add(undefined, 200));
 
-// parameter를 object로
+// parameter를 object로 전달하는 방법
 function add({a = 10, b = 20}) {
     return a + b;
 }
 
 console.log(add({b: 5}));
 
-// callback 함수 : 함수 내 혹은 인자에서 함수를 호출
+// callback 함수 : 인자로 함수를 전달 혹은 함수 내에서 함수를 호출
 function ask(question, yes, no) {
-    if (confirm(question) == true) yes()
+    if (confirm(question) == true) yes()    // ask 함수 내에서 window 내장 함수인 confirm() 호출
     else no();
 }
 
@@ -51,7 +51,7 @@ function showCancel() {
     alert( "거절 하셨습니다." );
 }
 
-ask("동의하십니까?", showOk, showCancel);
+ask("동의하십니까?", showOk, showCancel);   // 전달 인자로 showOk, showCancel 함수 전달
 
 // 재귀 함수 : 자기 자신을 내부에서 호출
 let cnt = 1;
