886ac8712cebee7fc3fdbbb86994d7f9ef1a48e6
OhRaeKyu <dhforb1234@naver.com>
2021-12-02 17:55:43 +0900

    2021-12-02

---
+++ 4_JavaScript/javascript/js/020.js
@@ -0,0 +1,26 @@
+let s = new Set('abcdeeeeeeeee');
+console.log(s);
+console.log(s.size);
+
+// Set에 값을 추가하기
+s.add('f');
+console.log(s);
+
+// Set을 순환하기
+for (let variable of s) {
+  console.log(variable);
+}
+
+// 값이 배열인 경우
+let ss = new Set('abcdeeeeeeeee'.split(''));
+console.log(ss);
+
+// Set의 값을 제거하기
+ss.delete('b');
+
+// Set의 값을 확인하기
+console.log(ss.has('a'));
+
+// Set의 모든 값을 제거하기 
+ss.clear();
+console.log(ss);
