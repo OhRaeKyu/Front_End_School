bfd8ed2686e698f2232bb5e82d1746e6a0530182
OhRaeKyu <dhforb1234@naver.com>
2021-12-06 16:24:30 +0900

    2021-12-06

--- 4_JavaScript/javascript/js/024.js
+++ 4_JavaScript/javascript/js/024.js
@@ -3,42 +3,50 @@ const redBtn = document.querySelector(".redBtn");
 const blueBtn = document.querySelector(".blueBtn");
 const yellowBtn = document.querySelector(".yellowBtn");
 
-// function colorChange(color) {
-//     for (let i of light.classList) {
-//         if (i !== color) {
-//             light.classList.remove(i);
-//         }
-//     }
-//     light.classList.add(color);
-// }
-
-// redBtn.addEventListener("click", colorChange("red"));
-// blueBtn.addEventListener("click", colorChange("blue"));
-// yellowBtn.addEventListener("click", colorChange("yellow"));
-
-redBtn.addEventListener("click", function() {
+function colorChange(color) {
     for (let i of light.classList) {
-        if (i !== "red") {
+        if (i !== color) {
             light.classList.remove(i);
         }
     }
-    light.classList.add("red");
+    light.classList.add(color);
+}
+
+redBtn.addEventListener("click", function() {
+    colorChange("red");
 });
 
 blueBtn.addEventListener("click", function() {
-    for (let i of light.classList) {
-        if (i !== "blue") {
-            light.classList.remove(i);
-        }
-    }
-    light.classList.add("blue");
+    colorChange("blue");
 });
 
 yellowBtn.addEventListener("click", function() {
-    for (let i of light.classList) {
-        if (i !== "yellow") {
-            light.classList.remove(i);
-        }
-    }
-    light.classList.add("yellow");
+    colorChange("yellow");
 });
+
+// redBtn.addEventListener("click", function() {
+//     for (let i of light.classList) {
+//         if (i !== "red") {
+//             light.classList.remove(i);
+//         }
+//     }
+//     light.classList.add("red");
+// });
+
+// blueBtn.addEventListener("click", function() {
+//     for (let i of light.classList) {
+//         if (i !== "blue") {
+//             light.classList.remove(i);
+//         }
+//     }
+//     light.classList.add("blue");
+// });
+
+// yellowBtn.addEventListener("click", function() {
+//     for (let i of light.classList) {
+//         if (i !== "yellow") {
+//             light.classList.remove(i);
+//         }
+//     }
+//     light.classList.add("yellow");
+// });
