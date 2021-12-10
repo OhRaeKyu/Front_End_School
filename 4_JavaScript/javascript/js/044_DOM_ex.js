422f486bace880131bd85f493627f2ee7b9cd8af
OhRaeKyu <dhforb1234@naver.com>
2021-12-10 15:50:59 +0900

    2021-12-10

--- 4_JavaScript/javascript/js/044_DOM_ex.js
+++ 4_JavaScript/javascript/js/044_DOM_ex.js
@@ -1,17 +1,40 @@
 const selectBtn = document.querySelector(".btn-select");
 const selectList = document.querySelector(".list-member");
+const languageList = ["Python", "Java", "JavaScript", "C#", "C++"];
+// let toggle = false;
+
+for (const language of languageList) {
+    const elementLi = document.createElement("li");
+    const elementBtn = document.createElement("button");
+    elementBtn.setAttribute("type", "button");
+    elementBtn.textContent += language;
+    selectList.appendChild(elementLi).appendChild(elementBtn);
+
+    // 좋지 않은 코드 : innerHTML의 비용이 많이들어 좋지 않다.
+    // selectList.innerHTML += `<li><button type="button">${iterator}</button></li>`;
+}
 
 selectBtn.addEventListener("click", function() {
-    selectList.style.display = "block";
-    selectList.innerHTML = `<li><button type="button">Python</button></li>
-    <li><button type="button">Java</button></li>
-    <li><button type="button">JavaScript</button></li>
-    <li><button type="button">C#</button></li>
-    <li><button type="button">C/C++</button></li>`;
+    selectBtn.classList.toggle("on");
 
+    // 좋지 않은 코드 : js에서 스타일을 건드는 것은 좋지 않다.
+    // if (toggle === false) {
+    //     selectList.style.display = "block";
+    //     toggle = true;
+    // }
+    // else {
+    //     selectList.style.display = "none";
+    //     toggle = false;
+    // }
 });
 
 selectList.addEventListener("click", function(event) {
-    selectList.style.display = "none";
-    selectBtn.innerText = event.target.innerText;
+    if (event.target.tagName === "BUTTON") {
+        selectBtn.classList.remove("on");
+        selectBtn.textContent = event.target.innerText;
+    }
+    
+    // 좋지 않은 코드 : js에서 스타일을 건드는 것은 좋지 않다.
+    // selectList.style.display = "none";
+    // toggle = false;
 })
